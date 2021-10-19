18f23bb7a2e36c29e9b7c4b69489565e94871272
”sovsianykov” <swebproger@gmail.com>
2021-10-19 11:34:38 +0300

    initial

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY_3 = "6d6c6a20e28a850dde6ce3c5120db654";
+
+export const API_KEY_4 =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDZjNmEyMGUyOGE4NTBkZGU2Y2UzYzUxMjBkYjY1NCIsInN1YiI6IjYwMjQzZTQyYzJiOWRmMDAzZmExOGU0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DuUsX9b51EeZY7vBb3OpOqBsWmwxzG_AputhYWA34a0";
